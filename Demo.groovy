417e0a70e52a7dfcc82f5c0fd8834e9c878de5eb
Vladimir Grichina <vgrichina@componentix.com>
2013-09-13 02:10:36 +0300

    Moved demo to separate file

---
+++ Demo.groovy
@@ -0,0 +1,21 @@
+def stemmer = new Stemmer()
+
+int total = 0
+int matches = 0
+new File("uk_stems.txt").eachLine {
+    if (!it) return;
+
+    def (word, expected) = it.split(",")
+    def stemmed = stemmer.stem(word)
+    def isMatching = (stemmed == expected)
+    println "${isMatching ? '✓' : '✗'} $word -> $stemmed \t$expected"
+
+    if (isMatching) matches++
+    total++
+}
+
+println "Total words: $total"
+println "Total matches: $matches"
+def accuracy = (float)matches / total * 100
+println "Accuracy: $accuracy%"
+
